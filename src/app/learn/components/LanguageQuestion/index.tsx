4de1a03a56b39dbd963092e6a4e6befca3f48231
Тимерьян Насибуллин <-github-email@example.com>
2023-11-25 00:08:56 +0500

    feat: add dd

--- src/app/learn/components/LanguageQuestion/index.tsx
+++ src/app/learn/components/LanguageQuestion/index.tsx
@@ -1,7 +1,86 @@
 "use client";
 
-const LanguageQuestion = () => {
-  return <></>;
+import { Button, Flex, Text } from "@radix-ui/themes";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+interface LanguageQuestionProps {
+  correctWord: string;
+  options: string[];
+  string: string;
+}
+
+const WordBtn = styled.div`
+  padding: 8px 12px;
+  border-radius: 4px;
+  cursor: grab;
+  box-shadow: 1px 1px 5px #00000020;
+`;
+
+const EmptyWordBox = styled(WordBtn)`
+  cursor: alias;
+  min-width: 50px;
+  min-height: 25px;
+`;
+
+const LanguageQuestion: React.FC<LanguageQuestionProps> = ({
+  correctWord,
+  options,
+  string,
+}) => {
+  const [draggedWord, setDraggedWord] = useState<string | null>(null);
+
+  const handleDragStart = (event: React.DragEvent<HTMLDivElement>, word: string) => {
+    event.dataTransfer.setData("text/plain", word);
+    setDraggedWord(word);
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const droppedWord = event.dataTransfer.getData("text/plain");
+    setDraggedWord(null);
+
+    // Проверяем, что слово не было перетащено в слот ранее и что это не пустое слово
+    if (droppedWord && droppedWord !== correctWord) {
+      const slot = document.getElementById("slot-for-text");
+      if (slot) {
+        slot.textContent = droppedWord;
+      }
+    }
+  };
+
+  return (
+    <Flex direction={"column"} gap={"3"}>
+      <Flex
+        direction={"column"}
+        gap={"3"}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+      >
+        <Flex gap={"1"} align={"center"}>
+          <Text>{string}</Text> <EmptyWordBox id="slot-for-text"></EmptyWordBox>
+        </Flex>
+
+        <Flex align={"center"} gap={"2"}>
+          {options.map((option, index) => (
+            <WordBtn
+              key={index}
+              draggable
+              onDragStart={(event) => handleDragStart(event, option)}
+            >
+              {option}
+            </WordBtn>
+          ))}
+        </Flex>
+      </Flex>
+
+      <Button>Подтвердить</Button>
+    </Flex>
+  );
 };
 
 export default LanguageQuestion;
--- src/app/learn/lesson/[id]/page.tsx
+++ src/app/learn/lesson/[id]/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import * as Progress from "@radix-ui/react-progress";
 import CodeQuestion from "../../components/CodeQuestion";
 import { useMemo, useState } from "react";
 
-const QUESTIONS_MOCK = [{ value: "console.log(1)", type: "code" }];
+import s from "./lesson.module.css";
+import WordDrop from "../../components/LanguageQuestion";
+import { Flex } from "@radix-ui/themes";
+
+const QUESTIONS_MOCK = [
+  { value: "console.log(1)", type: "code" },
+  { value: "Заполни конец", type: "lang" },
+];
 
 const LessonPage = () => {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(1);
 
   const currentQuestion = useMemo(
     () => QUESTIONS_MOCK[currentQuestionIndex],
@@ -14,9 +22,26 @@ const LessonPage = () => {
   );
 
   return (
-    <div style={{ height: "100%" }}>
-      {currentQuestion.type === "code" ? <CodeQuestion /> : <></>}
-    </div>
+    <Flex style={{ height: "100%" }} direction={"column"} gap={"3"}>
+      <Progress.Root className={s.progressbar} value={10}>
+        <Progress.Indicator
+          className={s.progressbar__indicator}
+          style={{ transform: `translateX(-${100 - 10}%)` }}
+        />
+      </Progress.Root>
+
+      {currentQuestion.type === "code" ? (
+        <CodeQuestion />
+      ) : currentQuestion.type === "lang" ? (
+        <WordDrop
+          string={currentQuestion.value}
+          correctWord="asd"
+          options={["моего хуя", "ыыыыы"]}
+        />
+      ) : (
+        <></>
+      )}
+    </Flex>
   );
 };
 
